b0f93c9b933c428c26a0df087e0888cb7b7f4271
blfuentes <blfuentes@mundo-r.com>
2023-11-12 19:32:31 +0100

    tweak 06-light-theremin
    - added extra control with pot for max frecuency

--- 06-light-theremin/src/main.cpp
+++ 06-light-theremin/src/main.cpp
@@ -1,12 +1,16 @@
 #include <Arduino.h>
 
 // define pins for the theremin and pins
-const int thereminPin = 8;
 const int sensorPin = A0;
+const int potPin = A1;
+const int thereminPin = 8;
 const int ledPin = 13;
 
 int sensorValue;
 int pitchValue;
+
+int potValue;
+int maxFrecuency;
 
 // variables to calibrate the sensor
 int sensorLow = 1023;
@@ -37,8 +41,15 @@ void setup() {
 }
 
 void loop() {
+  // read the potentiometer
+  potValue = analogRead(potPin);
+  maxFrecuency = map(potValue, 0, 1023, 1000, 10000);
+
+  // read the light sensor
   sensorValue = analogRead(sensorPin);
-  pitchValue = map(sensorValue, sensorLow, sensorHigh, 50, 4000);
+  pitchValue = map(sensorValue, sensorLow, sensorHigh, 50, maxFrecuency);
+
+  // play the tone
   tone(thereminPin, pitchValue, 20);
 
   delay(10);
