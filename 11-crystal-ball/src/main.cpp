92fae9e0272a02568737a14c2b2e1597debf4b19
blfuentes <blfuentes@mundo-r.com>
2023-11-16 20:32:42 +0100

    finished 11-crystal-ball

--- 11-crystal-ball/src/main.cpp
+++ 11-crystal-ball/src/main.cpp
@@ -1,18 +1,69 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// define the LCD object with pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+const int switchPin = 6;
+
+int switchState = 0;
+int prevSwitchState = 0;
+int reply;
+
+void setup()
+{
+  // configure the LCD
+  lcd.begin(16, 2);
+  pinMode(switchPin, INPUT);
+  lcd.print("Ask the");
+  lcd.setCursor(0, 1);
+  lcd.print("Crystal Ball");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  // read the switch
+  switchState = digitalRead(switchPin);
+  if (switchState != prevSwitchState && switchState == LOW)
+  {
+    // generate a random number
+    randomSeed(analogRead(0));
+    reply = random(8);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // print the reply
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("The ball says:");
+    lcd.setCursor(0, 1);
+    switch (reply)
+    {
+    case 0:
+      lcd.print("Yes");
+      break;
+    case 1:
+      lcd.print("No");
+      break;
+    case 2:
+      lcd.print("Ask again");
+      break;
+    case 3:
+      lcd.print("Maybe");
+      break;
+    case 4:
+      lcd.print("Not sure");
+      break;
+    case 5:
+      lcd.print("Doubtful");
+      break;
+    case 6:
+      lcd.print("No way");
+      break;
+    case 7:
+      lcd.print("Go for it");
+      break;
+    }
+  }
+
+  // save the switch state
+  prevSwitchState = switchState;
 }
