d70dfe0bc7a66024af567aade3df94da1dd400f1
blfuentes <blfuentes@mundo-r.com>
2023-11-21 21:02:27 +0100

    changes wowki diagram

--- 01-get-to-know-your-tools/src/main.cpp
+++ 01-get-to-know-your-tools/src/main.cpp
@@ -1,16 +1,15 @@
 #include <Arduino.h>
 
 const int ledPin = 13;
+const int pushButton = 2;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT);
+  pinMode(pushButton, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(ledPin, HIGH);
-  delay(1000);
-  digitalWrite(ledPin, LOW);
-  delay(1000);
+  digitalWrite(ledPin, digitalRead(pushButton));
 }
